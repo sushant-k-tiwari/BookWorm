09ce6ef417f0f6da306d94470ca671beff4bbc4c
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-13 22:52:02 +0530

    regiter api completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 connectDB()
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,12 +1,75 @@
 import express from "express";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
 const routes = express.Router();
 
+//generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+};
+
 //Register and Login Routes with POST
-routes.post("/register", (req, res) => {
-    res.send("Register User");
+routes.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    //checks on fields
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters" });
+    }
+
+    if (username.length < 3 || username.length > 20) {
+      return res
+        .status(400)
+        .json({ message: "Username must be between 3 to 20 characters" });
+    }
+
+    //check if user already exists
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const usernameExists = await User.findOne({ username });
+    if (usernameExists) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    //getting a random avatar from dicebear
+    const profileImage = `https://avatar.iran.liara.run/public`;
+
+    //creating new user
+    const newUser = new User({
+      username,
+      email,
+      password,
+      profileImage,
+    });
+
+    //saving new user
+    await newUser.save();
+
+    //generating token
+    const token = generateToken(newUser._id);
+    res
+      .status(201)
+      .json({ message: "User created successfully", data: newUser, token });
+  } catch (error) {
+    res.status(400).send("Error: " + error);
+  }
 });
 routes.post("/login", (req, res) => {
-    res.send("Login User");
+  res.send("Login User");
 });
 
 export default routes;
