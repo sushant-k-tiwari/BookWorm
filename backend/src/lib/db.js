25e8833b65beec934c68a5f9c79ee39feaab853a
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-13 22:51:35 +0530

    user db added

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Database Connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.log("Unable to connect to DB: ", error);
-        process.exit(1); //exiting with failure
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Database Connected`);
+  } catch (error) {
+    console.log("Unable to connect to DB: ", error);
+    process.exit(1); //exiting with failure
+  }
+};
