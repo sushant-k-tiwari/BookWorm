0c030fd62255f21ca1755d210f31e1c18aea941e
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-22 22:53:25 +0530

    book api's

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -3,4 +3,32 @@ import "dotenv/config";
 
 const router = express.Router();
 
+router.post("/", protectRoute, async (req, res) => {
+  try {
+    const { title, author, caption, rating, image } = req.body;
+
+    if (!image || !title || !author || !caption || !rating) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    //upload the image to cloudinary
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
+    //save to db
+    const newBook = new Book({
+        title,
+        author,
+        caption,
+        rating,
+        image: imageUrl,
+        user: req.user._id,
+    })
+    await newBook.save();
+    res.status(201).json({ message: "Book added successfully", newBook });
+
+  } catch (error) {
+    console.log("Error adding book", error);
+    res.status(500).json({ message: "Unable to add book", error });
+  }
+});
 export default router;
