934db382313f155a158a63a67f384963921873fc
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-23 11:29:44 +0530

    cors added

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,15 @@ import "dotenv/config";
 import authRoutes from "./routes/authRoutes.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
-
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
