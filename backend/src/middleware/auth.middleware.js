2a31cfa13252ed1db1a68f371be26c92255acb57
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2025-09-23 11:29:18 +0530

    auth middleware added

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -2,5 +2,31 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import "dotenv/config";
 
-const protectRoute = async (req, res, next) => {};
+const protectRoute = async (req, res, next) => {
+  try {
+    //get token
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "No Authorization token, access denied!" });
+    }
+    //verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    //find user
+    const user = await User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.status(401).json({ message: "Token is not valid!" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log("Error in protectRoute middleware", error);
+    res.status(500).json({ message: "Token is not valid" });
+  }
+};
+
+export default protectRoute;
